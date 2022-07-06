b28b450471c9382c898650ee0bbd570f77cc602b
ViacheslavDedov <dedoff.v@gmail.com>
2022-07-06 15:14:53 +0300

    add functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,33 +5,55 @@
 // import Notification from "./Notification";
 
 
+// export class App extends Component {
+//     state = {
+//         good: 0,
+//         neutral: 0,
+//         bad: 0
+//     }
 
+//     countTotalFeedback() {
+//         return this.state.good + this.state.neutral + this.state.bad;
+//     }
 
-// export const App = () => {
-//   return (
-    // <>
-    // <Section title="Please leave feedback">
+//     countPositiveFeedbackPercentage() { 
+//         return this.state.good / this.countTotalFeedback();
+//     }
 
-    //     <FeedbackOptions 
-    //         // options={options} 
-    //         // onLeaveFeedback={onLeaveFeedback}>
-    //     />
-        
-    // </Section>
+//     onLeaveFeedback() {
+//         return ;
+//     }
 
-    // <Section title="Statistic">
-        
-    //     <Statistics 
-    //         good={good}
-    //         neutral={neutral} 
-    //         bad={bad} 
-    //         total={total} 
-    //         positivePercentage={positivePercentage}
-    //     />
-          
-    //       <Notification message="There is no feedback"/>
+// render() {
+//         const {good, neutral, bad} = this.state;
+//         const total = this.countTotalFeedback();
+//         const positivPercentage = this.countPositiveFeedbackPercentage();
 
-    //   </Section>
-    // </>
-//   );
+//     return (
+//         <>
+//         <Section title="Please leave feedback">
+
+//             <FeedbackOptions 
+//                 options={['good', 'neutral', 'bad']} 
+//                 onLeaveFeedback={this.onLeaveFeedback}
+//             />
+            
+//         </Section>
+
+//         <Section title="Statistic">
+//             {this.countTotalFeedback ()> 0 ?
+//                 (<Statistics 
+//                 good={good}
+//                 neutral={neutral} 
+//                 bad={bad} 
+//                 total={total} 
+//                 positivePercentage={positivePercentage} />)
+//                 : (<Notification message="There is no feedback" />)
+//                 }
+            
+//         </Section>
+//         </>
+//         );
+//     }
+  
 // };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,6 +2,3 @@ import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
 
-// countTotalFeedback()
-
-// countPositiveFeedbackPercentage()
